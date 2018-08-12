17f0d56f25163d44c77809d6cb1f9b4cde4061e3
Adam Ophoff <aophof46@gmail.com>
2018-08-11 22:04:16 -0400

    add cache removal

--- RunChrome.sh
+++ RunChrome.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-
 FILE="./RunChrome.env"
 SERVICE="chromium"
 RESULT=`pgrep ${SERVICE}`
 URL=$(cat "$FILE")
 if [ "${RESULT:-null}" = null ]; then
+	`rm -r ~/.cache/chromium/Default/Cache/*`
 	`/usr/bin/chromium-browser ${URL}`
 else
 	exit
