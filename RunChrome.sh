1d02550b2e0c3360908981e0b8ca042cc577f57b
Adam <aophof46@users.noreply.github.com>
2018-09-02 22:50:49 -0400

    Update RunChrome.sh

--- RunChrome.sh
+++ RunChrome.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 DIR="$( dirname "$(readlink -f "$0")" )"
 FILE="RunChrome.env"
+FLAGS="--disable-extensions --disable-session-crashed-bubble --disable-infobars"
 SERVICE="chromium"
 RESULT=`pgrep ${SERVICE}`
 URL=$(cat $DIR/$FILE)
 if [ "${RESULT:-null}" = null ]; then
         `rm -r ~/.cache/chromium/Default/Cache/*`
-        `/usr/bin/chromium-browser ${URL}`
+        `/usr/bin/chromium-browser ${FLAGS} ${URL}`
 else
         exit
 fi
