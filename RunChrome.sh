8548ff5b9172a8c7002c80f0d81cc9b1536724f6
Adam <aophof46@users.noreply.github.com>
2018-09-02 23:05:33 -0400

    Update RunChrome.sh

--- RunChrome.sh
+++ RunChrome.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DIR="$( dirname "$(readlink -f "$0")" )"
 FILE="RunChrome.env"
-FLAGS="--disable-extensions --disable-session-crashed-bubble --disable-infobars"
+FLAGS="--disable-extensions --disable-session-crashed-bubble --disable-infobars --start-maximized"
 SERVICE="chromium"
 RESULT=`pgrep ${SERVICE}`
 URL=$(cat $DIR/$FILE)
