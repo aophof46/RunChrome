a30094f7690f99a8da37c7c173b37df4fb2f5855
aophof46 <aophof46@gmail.com>
2019-03-31 23:12:33 -0400

    added killall statement, cleaned up echo commands.

--- detect.sh
+++ detect.sh
@@ -7,22 +7,27 @@ BROWSER="chromium-browser-v7"
 
 # Detect whether someone is connected in via VNC.  This will prevent the script from killing the browser
 # when someone is viewing it while logged in for troubleshooting
-#RESULT=`netstat -p -t tcp | grep vncserver-x11-c | grep -v localhost | wc -l`
-RESULT=0
+VNCRESULT=`netstat -p -t tcp | grep vncserver-x11-c | grep -v localhost | wc -l`
+#VNCRESULT=0
 
-if [ $RESULT = 0 ]; then
+if [ $VNCRESULT = 0 ]; then
 	# No VNC sessions currently running
-        echo "No VNC sessions detected"
+        #echo "No VNC sessions detected"
         USAGE=`ps aux | grep $BROWSER | awk '{ print $3}' | awk '{total = total + $1}END{print total}'`
         USAGE=$( printf "%.0f" $USAGE )
-        echo $USAGE
-        if [ $USAGE -lt $THRESHOLD ]; then
-                echo "Below threshold"
+        #echo $USAGE
+	if [ $USAGE = 0 ]; then
+		#echo "$BROWSER process not found"
+		exit
+        elif [ $USAGE -lt $THRESHOLD ]; then
+		#echo "Below threshold"
+		killall $BROWSER
         else
-                echo "Above threshold"
+                #echo "Above threshold"
+		exit
         fi
 else
 	# At least one VNC session connected
-	echo "VNC sessions detected"
+	#echo "VNC sessions detected"
 	exit
 fi
