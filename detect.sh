3be9546d83f324213e8a558be4682718ece004c8
aophof46 <aophof46@gmail.com>
2019-03-31 22:59:25 -0400

    Added detection script to kill crashed chromium browser sessions

---
+++ detect.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# CPU Threshold.  CPU about this amount should indicate an active browser
+THRESHOLD=60
+
+# Browser process name.
+BROWSER="chromium-browser-v7"
+
+# Detect whether someone is connected in via VNC.  This will prevent the script from killing the browser
+# when someone is viewing it while logged in for troubleshooting
+#RESULT=`netstat -p -t tcp | grep vncserver-x11-c | grep -v localhost | wc -l`
+RESULT=0
+
+if [ $RESULT = 0 ]; then
+	# No VNC sessions currently running
+        echo "No VNC sessions detected"
+        USAGE=`ps aux | grep $BROWSER | awk '{ print $3}' | awk '{total = total + $1}END{print total}'`
+        USAGE=$( printf "%.0f" $USAGE )
+        echo $USAGE
+        if [ $USAGE -lt $THRESHOLD ]; then
+                echo "Below threshold"
+        else
+                echo "Above threshold"
+        fi
+else
+	# At least one VNC session connected
+	echo "VNC sessions detected"
+	exit
+fi
